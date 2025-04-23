b606c8c08980d580379a7a3a6a5ddda5aeca4604
LordVader104 <ergenekonf@gmail.com>
2025-04-23 14:06:02 +0300

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,9 @@ module com.example.main_screen {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires com.fasterxml.jackson.databind;
 
-    opens com.example.main_screen to javafx.fxml;
+    opens com.example.main_screen to com.fasterxml.jackson.databind;
+
     exports com.example.main_screen;
 }
